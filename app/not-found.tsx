f142cbc398d99c4dbf19f7816ec257b878d8bdb9
Leonardo <Leonardo.Zuliani@etu.univ-grenoble-alpes.fr>
2025-05-18 14:27:56 +0200

    Changed @components/general/SideThing to ./components/general/SideThing.jsx. Because not-found is not in app.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import SideThing from "@components/general/SideThing";
+import SideThing from "./components/general/SideThing.jsx";
 import Link from "next/link";
 
 export default function NotFound() {
