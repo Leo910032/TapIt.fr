d0a90b081858698ef3d4bb33f209a212436188bd
Leonardo <Leonardo.Zuliani@etu.univ-grenoble-alpes.fr>
2025-05-20 23:16:06 +0200

    modified:   app/(forget password pages)/forgot-password/page.jsx

--- app/(forget password pages)/forgot-password/page.jsx
+++ app/(forget password pages)/forgot-password/page.jsx
@@ -1,8 +1,9 @@
+// Server Component
 import { Toaster } from "react-hot-toast";
 import ForgotPasswordForm from "./componets/ForgotPasswordForm";
 import SideThing from "@/app/components/General Components/SideThing";
 
-export const generateMetadata = () =>{
+export const generateMetadata = () => {
     return {
         title: "Link Tree | Forgot password",
         description: "Use this form to get a reset password url"
@@ -10,9 +11,6 @@ export const generateMetadata = () =>{
 }
 
 export default function ForgotPasswordPage() {
-    const router = useRouter();
-
-
     return (
         <div className="flex h-screen w-screen">
             <Toaster />
